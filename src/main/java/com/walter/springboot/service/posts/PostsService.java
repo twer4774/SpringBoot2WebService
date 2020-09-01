f8c13ff388bb4f2e872d124171a34d239f4f664d
walter <twer4774@gmail.com>
2020-09-01 20:51:47 +0900

    feat: Modify post && Delete post

--- src/main/java/com/walter/springboot/service/posts/PostsService.java
+++ src/main/java/com/walter/springboot/service/posts/PostsService.java
@@ -58,4 +58,12 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    //삭제
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        postsRepository.delete(posts);
+    }
 }
